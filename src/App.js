e2e9e969a689396b3565447721014b602734a9c8
Kasra <kasra76@gmail.com>
2021-04-29 12:12:20 +0430

    dynamic route to blog pages added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 
 import React from 'react';
 import './App.css';
-import Home from './components/pages/HomePage/Home';
-import Services from './components/pages/Services/Services';
-import Products from './components/pages/Products/Products';
-import Team from './components/pages/Team/Team'
-import SignUp from './components/pages/SignUp/SignUp';
-import Blog from './components/pages/blog/Blog'
+import Blog from './components/pages/blog/Blog';
+import BlogPage from './components/pages/blog/BlogPage'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/pages/Footer/Footer';
@@ -33,7 +29,8 @@ function App() {
       <Navbar />
         <Switch>
           <Route path="/" exact component={ Index } />
-          <Route path="/blog" component={ Blog } />
+          <Route path="/blog" exact component={ Blog } />
+          <Route path="/blog/:id" component={ BlogPage }/>
         </Switch>
       <Footer />
     </Router>
--- src/components/pages/blog/Blog.js
+++ src/components/pages/blog/Blog.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
 
 function Blog() {
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const [items, setItems] = useState([])
+
+    const fetchData = async () => {
+        const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+        const items = await response.json();
+
+        console.log(items);
+        setItems(items);
+    }
+
     return (
-        <div className="blog-container">
-            <h1>HIiiii</h1>
+        <div className="blogs-container">
+            {
+                items.map(item => (
+                    <h2 key={item.id}>
+                        <Link to={`/blog/${item.id}`}>{item.title}</Link>
+                    </h2>
+                ))
+            }
         </div>
-    );
-}
+    )
+};
 
 export default Blog;
