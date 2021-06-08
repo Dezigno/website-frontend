23c2ccb27d64693cb41b138e978f2c51cd7764b6
Kasra <kasra76@gmail.com>
2021-06-08 21:24:48 +0430

    shop page container and grids fixed

--- src/components/pages/Shop/Shop.js
+++ src/components/pages/Shop/Shop.js
@@ -2,19 +2,24 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Layout from './Layout'
 import  Navbar from './navbar/Navbar'
 // material ui components
-import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core'
+import { Container, createMuiTheme, makeStyles, ThemeProvider, Typography } from '@material-ui/core'
 import { purple } from '@material-ui/core/colors'
-
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+// CSS
+import './shop.css'
 
 const useStyles = makeStyles((theme) => {
   return (
     {
       banner: {
-        // width: "100%",
-        height: "600px",
         backgroundImage: 'url("/test/bg-banner.jpeg")',
-        backgroundPosition: "center top",
-        backgroundSize: 'cover'
+      }, 
+      productImage: {
+        backgroundImage: 'url("/test/product/shirt.jpg")',
+      },
+      itemGrid: {
+        marginBottom: '60px'
       }
     }
   );
@@ -27,7 +32,126 @@ function Shop() {
     <div>
       <Navbar />
       <div className="main-content">
-        <div className={classes.banner} />
+        <div className={`${classes.banner} banner`} />
+        <div className="title-heading">
+          <h2>محصولات ویژه</h2>
+        </div>
+        <Container >
+          <Grid container spacing={2}>
+            <Grid item  xs={12} sm={6} md={4} lg={3} xl={2} className={classes.itemGrid}>
+              <Paper elevation={0} className={classes.paper}>
+                <div className={`${classes.productImage} product-image`} />
+                <div className="product-details">
+                  <div className="product-title">
+                    <h4>تیشرت نویز های قرمز</h4>
+                  </div>
+                  <div className="product-type">
+                    <p>تی‌شرت</p>
+                  </div>
+                  <div className="product-price">
+                    <b>۱۰۰,۰۰۰ تومان</b>
+                  </div>
+                </div>
+              </Paper>
+            </Grid>
+            <Grid item  xs={12} sm={6} md={4} lg={3} xl={2} className={classes.itemGrid}>
+              <Paper elevation={0} className={classes.paper}>
+                <div className={`${classes.productImage} product-image`}></div> 
+                <div className="product-details">
+                  <div className="product-title">
+                    <h4>تیشرت نویز های قرمز</h4>
+                  </div>
+                  <div className="product-type">
+                    <p>تی‌شرت</p>
+                  </div>
+                  <div className="product-price">
+                    <b>۱۰۰,۰۰۰ تومان</b>
+                  </div>
+                </div>
+              </Paper>
+            </Grid>
+            <Grid item  xs={12} sm={6} md={4} lg={3} xl={2} className={classes.itemGrid}>
+              <Paper elevation={0} className={classes.paper}>
+                <div className={`${classes.productImage} product-image`}></div> 
+                <div className="product-details">
+                  <div className="product-title">
+                    <h4>تیشرت نویز های قرمز</h4>
+                  </div>
+                  <div className="product-type">
+                    <p>تی‌شرت</p>
+                  </div>
+                  <div className="product-price">
+                    <b>۱۰۰,۰۰۰ تومان</b>
+                  </div>
+                </div>
+              </Paper>
+            </Grid>
+            <Grid item  xs={12} sm={6} md={4} lg={3} xl={2} className={classes.itemGrid}> 
+              <Paper elevation={0} className={classes.paper}>
+                <div className={`${classes.productImage} product-image`}></div> 
+                <div className="product-details">
+                  <div className="product-title">
+                    <h4>تیشرت نویز های قرمز</h4>
+                  </div>
+                  <div className="product-type">
+                    <p>تی‌شرت</p>
+                  </div>
+                  <div className="product-price">
+                    <b>۱۰۰,۰۰۰ تومان</b>
+                  </div>
+                </div>
+              </Paper>
+            </Grid>
+            <Grid item  xs={12} sm={6} md={4} lg={3} xl={2} className={classes.itemGrid}>
+              <Paper elevation={0} className={classes.paper}>
+                <div className={`${classes.productImage} product-image`}></div> 
+                <div className="product-details">
+                  <div className="product-title">
+                    <h4>تیشرت نویز های قرمز</h4>
+                  </div>
+                  <div className="product-type">
+                    <p>تی‌شرت</p>
+                  </div>
+                  <div className="product-price">
+                    <b>۱۰۰,۰۰۰ تومان</b>
+                  </div>
+                </div>
+              </Paper>
+            </Grid>
+            <Grid item  xs={12} sm={6} md={4} lg={3} xl={2} className={classes.itemGrid}>
+              <Paper elevation={0} className={classes.paper}>
+                <div className={`${classes.productImage} product-image`}></div> 
+                <div className="product-details">
+                  <div className="product-title">
+                    <h4>تیشرت نویز های قرمز</h4>
+                  </div>
+                  <div className="product-type">
+                    <p>تی‌شرت</p>
+                  </div>
+                  <div className="product-price">
+                    <b>۱۰۰,۰۰۰ تومان</b>
+                  </div>
+                </div>
+              </Paper>
+            </Grid>
+            <Grid item  xs={12} sm={6} md={4} lg={3} xl={2} className={classes.itemGrid}> 
+              <Paper elevation={0} className={classes.paper}>
+                <div className={`${classes.productImage} product-image`}></div> 
+                <div className="product-details">
+                  <div className="product-title">
+                    <h4>تیشرت نویز های قرمز</h4>
+                  </div>
+                  <div className="product-type">
+                    <p>تی‌شرت</p>
+                  </div>
+                  <div className="product-price">
+                    <b>۱۰۰,۰۰۰ تومان</b>
+                  </div>
+                </div>
+              </Paper>
+            </Grid>
+          </Grid>
+        </Container>
       </div>
     </div>
   );
